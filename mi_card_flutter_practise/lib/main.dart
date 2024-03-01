16cb033c3d907c505709c744405ec4425acbcd14
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2024-03-01 20:15:55 +0500

    mi_card project completed task:3

--- mi_card_flutter_practise/lib/main.dart
+++ mi_card_flutter_practise/lib/main.dart
@@ -12,35 +12,79 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.red,
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 300, 0, 0),
-                child: Column(
-                  children: [
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.yellow,
-                    ),
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.green,
-                    ),
-                  ],
+              Center(
+                child: CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/icon.jpg'),
                 ),
               ),
-              Container(
-                width: 100.0,
-                height: double.infinity,
-                color: Colors.blue,
+              SizedBox(height: 10.0,),
+              Text(
+                'Khizar H.S',
+                style: TextStyle(
+                  fontFamily: 'Anta',
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSans3',
+                  fontSize: 17.0,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 180.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
+                child: ListTile(
+                  leading: Icon(
+                      Icons.phone,
+                      size: 30.0,
+                      color: Colors.teal.shade400,
+                    ),
+                  title: Text(
+                      '+92 317 8906226',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'SourceSans3',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.0,
+                      ),
+                    ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
+                child: ListTile(
+                  leading: Icon(
+                      Icons.email_outlined,
+                      size: 30.0,
+                      color: Colors.teal.shade400,
+                    ),
+                  title: Text(
+                      'khizarsiddiqui10@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'SourceSans3',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.0,
+                      ),
+                    ),
+                ),
               ),
             ],
           ),
