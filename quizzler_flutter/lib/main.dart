2d4ba7f58aaf5c303caeaf2eb333e35d924c5a36
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2024-03-06 18:09:26 +0500

    quizzler app in progress

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -27,6 +27,17 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -39,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -66,7 +77,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == true){
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
                 setState(() {
+                  questionNumber++;
                   scoreKeeper.add(
                     Icon(
                       Icons.check, 
@@ -94,6 +112,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == true){
+                  print('user got it right');
+                } else {
+                  print('user got it wrong');
+                }
+                questionNumber++;
                 setState(() {
                   scoreKeeper.add(
                     Icon(
